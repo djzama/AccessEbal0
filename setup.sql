e68025767e4f2bb455b64989b253a5a33dff37b1
djzama <djzamareturns@gmail.com>
2023-02-10 16:27:25 +0300

    docker mysql

--- setup.sql
+++ setup.sql
@@ -1,7 +1,10 @@
+CREATE DATABASE `IdenFaces1`;
 
+USE `IdenFaces1`;
 --- Faces tables
 CREATE TABLE faces (
     id INT AUTO_INCREMENT PRIMARY KEY,
     face BLOB
 );
 
+INSERT INTO `IdenFaces1` VALUES
